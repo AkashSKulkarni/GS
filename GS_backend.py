30b0a45ed713d81b7c1f729deb4f94f8f0c17a47
AkashSKulkarni <132072223+AkashSKulkarni@users.noreply.github.com>
2024-08-05 21:49:34 +0530

    Update GS_backend.py

--- GS_backend.py
+++ GS_backend.py
@@ -15,7 +15,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import chromedriver_autoinstaller
 from webdriver_manager.core.os_manager import ChromeType
-
+from selenium.webdriver.chrome.options import Options
 import nltk
 from nltk.corpus import stopwords
 
