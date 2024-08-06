bc9ab3bc4f4e54c8d8f4bc18441e7862a615c748
AkashSKulkarni <132072223+AkashSKulkarni@users.noreply.github.com>
2024-08-06 10:45:31 +0530

    Update GS_frontend.py

--- GS_frontend.py
+++ GS_frontend.py
@@ -1,4 +1,4 @@
-'''import streamlit as st
+import streamlit as st
 import pandas as pd
 import xlsxwriter
 import GS_backend
@@ -16,30 +16,9 @@ if st.button('Search'):
     df_linkedin = pd.DataFrame(linkedin_profiles)
     df_linkedin = df_linkedin[['Name', 'Title', 'Location', 'Experience', 'Education', 'Skills','LinkedIn_URL','Phone','Email']]
     
-    st.dataframe(df_linkedin , use_container_width=True, width=1200,hide_index=True)'''
+    st.dataframe(df_linkedin , use_container_width=True, width=1200,hide_index=True)
 
-import streamlit as st
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
-def test_chromedriver():
-    try:
-        options = Options()
-        options.add_argument("--headless")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--no-sandbox")
-        
-        # Corrected the way options are passed to the Chrome driver
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-        driver.get("https://www.google.com")
-        st.write(driver.title)
-        driver.quit()
-    except Exception as e:
-        st.error(f"Error running ChromeDriver: {e}")
-
-st.title("Test ChromeDriver on Streamlit Cloud")
-test_chromedriver()
 
 
     
